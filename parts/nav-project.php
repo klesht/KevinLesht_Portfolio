288c1c2885baf9fb2ae92680192ce79fdac7d7d7
Kevin Lesht <klesht@gmail.com>
2015-06-10 19:24:11 -0500

    Project nav updated for MQ.

--- parts/nav-project.php
+++ parts/nav-project.php
@@ -15,21 +15,21 @@
 
 		<?php if( get_adjacent_post(false, '', true) ) { ?>
 
-		<li><?php previous_post_link('%link', '<span class="fa fa-chevron-left"></span>&nbsp;&nbsp;&nbsp;Last Project'); ?></li>
+		<li><?php previous_post_link('%link', '<span class="fa fa-chevron-left"></span><span class="device-hidden">&nbsp;&nbsp;&nbsp;Last Project</span>'); ?></li>
 		
 		<?php } else { 
 		    $first = new WP_Query('posts_per_page=1&order=DESC&post_type=projects'); $first->the_post();
-		    	echo '<li><a href="' . get_permalink() . '"><span class="fa fa-chevron-left"></span>&nbsp;&nbsp;&nbsp;Last Project</a></li>';
+		    	echo '<li><a href="' . get_permalink() . '"><span class="fa fa-chevron-left"></span><span class="device-hidden">&nbsp;&nbsp;&nbsp;Last Project</span></a></li>';
 		  	wp_reset_query();
 		}; ?>
 		    
 		<?php if( get_adjacent_post(false, '', false) ) { ?>
 
-		<li><?php next_post_link('%link', 'Next Project&nbsp;&nbsp;&nbsp;<span class="fa fa-chevron-right"></span>'); ?></li>
+		<li><?php next_post_link('%link', '<span class="device-hidden">Next Project&nbsp;&nbsp;&nbsp;</span><span class="fa fa-chevron-right"></span>'); ?></li>
 		
 		<?php } else { 
 			$last = new WP_Query('posts_per_page=1&order=ASC&post_type=projects'); $last->the_post();
-		    	echo '<li><a href="' . get_permalink() . '">Next Project&nbsp;&nbsp;&nbsp;<span class="fa fa-chevron-right"></span></a></li>';
+		    	echo '<li><a href="' . get_permalink() . '"><span class="device-hidden">Next Project&nbsp;&nbsp;&nbsp;</span><span class="fa fa-chevron-right"></span></a></li>';
 		    wp_reset_query();
 		}; ?>
 
